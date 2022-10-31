9dd8c38762a7d07918e32633c5e52e87a3b175de
sindrebrurberg <sindre@brurberg.no>
2022-10-31 18:12:37 +0900

    Warning message

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -19,6 +19,7 @@
 <div class="content flex">
   <div class="item">
     <h1>Cantara`s annual christmas beer tasting</h1>
+    <h1 style="color: red">Under development, no data will be stored.</h1>
     <h4 style="text-align: center;">Cantara holds a beer tasting every year to determine what beers are the best Norwegian christmas beer.</h4>
     <p style="text-align: center;">There are a set of rules for the tasting and points are weighted based on if it is your first time or if you do certain things during the event.</p>
     <p style="text-align: center;">If you have brought a Norwegian beer that is missing from the list. Please ask this years selected newbie and have them add it.</p>
